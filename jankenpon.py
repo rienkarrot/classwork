47802f4f284192dd1e7b203cde3bd464b76059d6
Ri'en Karrot <rienkarrot@gmail.com>
2013-10-07 15:12:49 -0700

    rock paper scissors

---
+++ jankenpon.py
@@ -0,0 +1,17 @@
+import random
+teammate = finn
+
+turns = 0
+
+while turns < 6:
+    player_choice = raw_input('')
+    computer_choice = random.choice(['rock', 'paper', 'scissors'])
+	print computer_choice
+	turns +=1
+	
+	if player_choice != computer_choice:
+	    
+	else:
+	    print "It's a tie!"
+
+print choice
