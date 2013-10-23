7697da505f07dab4386cf62ed00867af10631de0
Ri'en Karrot <rienkarrot@gmail.com>
2013-10-22 19:21:57 -0700

    classwork - testing

--- lectures/lecture9/test.py
+++ lectures/lecture9/test.py
@@ -6,5 +6,23 @@ class TestSearch(unittest.TestCase):
     def test_sample(self):
         self.assertEqual(1, binarysearch.search([0,1,2], 1))
 
+    def test_five(self):
+        self.assertEqual(2, binarysearch.search([0,1,2,3,4], 2))
+    	
+    def test_one(self):
+        self.assertEqual(0, binarysearch.search([0], 0))
+    
+    def test_none(self):
+        self.assertEqual("", binarysearch.search([""], ""))
+    	
+    def test_words(self):
+        self.assertEqual(0, binarysearch.search(["zebra", "horse"], "zebra"))
+    
+    def test_lots(self):
+        self.assertEqual(4, binarysearch.search([0,5,10,15,20,25,30,35], 20))
+
+    def test_different(self):
+        self.assertEqual(
+	
 if __name__ == '__main__':
     unittest.main()
