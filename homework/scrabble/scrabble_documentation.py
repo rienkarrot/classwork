8626e74368f37baa7a5d29c681c7dad25deac0de
Ri'en Karrot <rienkarrot@gmail.com>
2013-12-10 19:14:29 -0800

    more homework

---
+++ homework/scrabble/scrabble_documentation.py
@@ -0,0 +1,32 @@
+with
+open("word_list.txt")
+as f:
+    #do something to read f
+'''
+word_list.txt is a file full of all-caps words, and they are all valid words.
+
+invalid
+helper function 1:
+    -word is not valid
+helper function 2:
+    -words are not connected to start square
+    
+helper function 3:
+'''
+def get_rows_and_columns_as_strings(board):
+'''
+helper function 4:
+'''
+def get_all_words(board):
+'''
+helper function 5:
+'''
+def check_all_words(board):
+'''
+helper function 6:
+'''
+def are_squares_connected(board):
+'''
+helper function 7:
+'''
+def mark_connected_squares(board):
--- homework/week5/anagram_test.py
+++ homework/week5/anagram_test.py
@@ -2,7 +2,24 @@ import unittest
 from anagram import *
 
 class TestAnagram(unittest.TestCase):
+'''
+Our recursive function lives in anagram.py.  Take a good look at that
+whole file, both the code and the spec.
 
-    def testExample(self):
+Your task: write unit tests for the following functions:
+
+ - `normalize`
+ - `remove_letters`
+ - `anagrams`
+ '''
+    def test_normalize(self):
+        self.assertEqual("a z q l p", "alpqz")
+
+    def test_remove_letters(self):
+        self.assertEqual(d, {'foo':['bar', 'bar', 'baz'], 'quux':['moose']})
         pass
-
+        
+    def test_anagrams(self):
+        self.assertEqual(d, {'foo':['bar', 'bar', 'baz'], 'quux':['moose']})
+        pass
+        
